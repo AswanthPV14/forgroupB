0b4c09e99332f1a06093bdf4d6adf1bc94de966e
sandeepaustin3 <sandeepaustin3@gmail.com>
2024-09-29 22:41:44 +0530

    Update urls.py

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ServiceRequestViewSet, BookingViewSet, ServiceRequestCreateView, BookingListView, BookingDetailView
+
+router = DefaultRouter()
+router.register(r'service-requests', ServiceRequestViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
-    #path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('', include(router.urls)),
+    path('service/request/', ServiceRequestCreateView.as_view(), name='service-request'),
+    path('bookings/', BookingListView.as_view(), name='bookings-list'),
+    path('bookings/<int:pk>/', BookingDetailView.as_view(), name='booking-detail'),
 ]
+
