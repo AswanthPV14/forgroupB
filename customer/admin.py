8a2861474ae2d20aed3a99d93e5887f8cdb7b03f
sandeepaustin3 <sandeepaustin3@gmail.com>
2024-09-29 22:55:25 +0530

    Update admin.py

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ServiceRequest, Customer, Booking
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(ServiceRequest)
+admin.site.register(Booking)
